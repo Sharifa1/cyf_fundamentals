cb41620c288fe2558008e8fc605156c63bb55d75
Sharifa1 <105314490+Sharifa1@users.noreply.github.com>
2022-05-30 23:36:21 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-var name2, comment;
+var name2, inquiry;
 
 function getNumberOrString(value) {
   // Convert a string value to a number if possible
@@ -12,17 +12,17 @@ function getNumberOrString(value) {
 
 
 
-document.getElementById('add_comment').addEventListener('click', (event) => {
+document.getElementById('make_inquiry').addEventListener('click', (event) => {
   name2 = getNumberOrString(document.getElementById('name').value);
-  comment = getNumberOrString(document.getElementById('comment').value);
-  let element_your_comment = document.getElementById('your_comment');
+  inquiry = getNumberOrString(document.getElementById('inquiry').value);
+  let element_previous_comment = document.getElementById('previous_comment');
   let new_li = document.createElement('li');
   new_li.innerText = name2;
   let new_ol = document.createElement('ol');
-  new_ol.innerText = comment;
+  new_ol.innerText = inquiry;
 
   new_li.appendChild(new_ol);
 
-  element_your_comment.appendChild(new_li);
+  element_previous_comment.appendChild(new_li);
 
 });
